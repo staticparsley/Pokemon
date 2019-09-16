355c4c28c64cefeaec644d12778f57772ced8f21
Ruben Marin <staticparsley92@gmail.com>
2019-09-16 13:32:05 -0400

    finished small project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokegame from './PokeGame';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
